d60762b1b705e04d5b3b3dcbc1a8c215a49f9f2a
Santiago Sánchez Mora <sashita2001@hotmail.com>
2022-10-06 01:56:58 -0500

    fix: 🐛 structure modified for pull request

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,8 @@
 import { NonEmptyArray } from 'type-graphql'
 import { PingResolver } from './ping'
-import { GradingResolver } from './grading'
 import { ProfileResolver } from './profile'
 
 export const resolvers: NonEmptyArray<Function> | NonEmptyArray<string> = [
   PingResolver,
-  GradingResolver,
   ProfileResolver
 ]
